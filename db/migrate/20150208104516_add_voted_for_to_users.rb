5ad69fe71260be72604bb9f469e51611b71fa412
BV Satyaram <bvsatyaram@gmail.com>
2015-02-08 19:30:33 +0530

    Fixing PG issue

--- db/migrate/20150208104516_add_voted_for_to_users.rb
+++ db/migrate/20150208104516_add_voted_for_to_users.rb
@@ -1,6 +1,6 @@
 class AddVotedForToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :voted_for, index: true
-    add_foreign_key :users, :voted_fors
+    add_foreign_key :users, :voted_for
   end
 end
