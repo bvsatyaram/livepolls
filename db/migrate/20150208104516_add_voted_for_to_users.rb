60b5a9d989be9d65c2d7d2cdd86175cfbf831a48
BV Satyaram <bvsatyaram@gmail.com>
2015-02-08 19:36:39 +0530

    Fixing index issue related to PG

--- db/migrate/20150208104516_add_voted_for_to_users.rb
+++ db/migrate/20150208104516_add_voted_for_to_users.rb
@@ -1,6 +1,6 @@
 class AddVotedForToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :voted_for, index: true
-    add_foreign_key :users, :voted_for
+    add_foreign_key :users, :users, column: :voted_for_id, primary_key: :id
   end
 end
